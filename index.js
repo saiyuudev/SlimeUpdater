23649916ae9d3c5f6c618b33e87be200acf9e40a
Saiyuu <sdk_ssh@protonmail.com>
2024-08-16 21:24:54 +0200

    [Add] Add config and working on website

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const express = require("express")
+const App = express()
+const cookieParser = require('cookie-parser')
+const formidable = require('express-formidable');
+
+console.log("Loading website...")
+
+App.set("views", "./templates")
+App.set("view engine", "ejs")
+App.use(express.static('public'));
+App.use(cookieParser())
+App.use(formidable({
+    encoding: 'utf-8',
+    multiples: true,
+    keepExtensions: true
+}));
+
